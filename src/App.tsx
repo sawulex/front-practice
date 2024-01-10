155eae7adbae2a08759e1be467ee857f8d32b9df
sawulex <sawulex76@gmail.com>
2024-01-10 22:52:42 +0300

    RedButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
+  const [clicks, setClicks] = React.useState(0);
+
+  function increaseClicks() {
+    setClicks(clicks + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <RedButton onClick={increaseClicks}>НЕ НАЖИМАТЬ!</RedButton>
+        <Counter>И все же ты нажал {clicks} раз</Counter>
       </header>
     </div>
   );
 }
 
 export default App;
+const RedButton = styled.button({
+  height: 200,
+  width: 200,
+  backgroundColor: '#E80000',
+  color: '#FFFFFF',
+  fontSize: 18,
+  fontWeight: 700,
+  borderRadius: '50%',
+  border: '3px solid #FFFFFF',
+  cursor: 'pointer',
+
+  '&:hover': {
+    backgroundColor: '#F80000',
+  },
+
+  '&:active': {
+    backgroundColor: '#D80000',
+  },
+});
+
+const Counter = styled.p({
+  fontSize: 24,
+  color: '#FFFFFF',
+});
